252bfd5481610f40a005880934dcecae1fd6426d
Avaer Kazmer <a@modules.io>
2018-07-17 21:16:17 -0400

    Clean up build script

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 source ~/emsdk/emsdk_env.sh
 export platform=emscripten
 export DEBUG=1
-make clean
+find | grep '\.o$' | xargs rm
+# make clean
 emmake make -f Makefile.libretro -j3
 cp pcsx_rearmed_libretro.so ~/RetroArch/dist-scripts/pcsx_rearmed_libretro_emscripten.bc
